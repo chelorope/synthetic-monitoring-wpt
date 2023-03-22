87a13181cf604e0de73dd00e109c306f344a95b5
chelorope <cmarcelorp@gmail.com>
2023-03-22 11:13:51 -0300

    Path sorting

--- test.js
+++ test.js
@@ -2,7 +2,8 @@ import * as dotenv from "dotenv";
 import WPT from "./WPTService.js";
 
 dotenv.config();
-const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiOTlmNTQ3ZGMtZmUwYS00ZGEyLWI3ZTQtZTgzZmUzZjA0OWRkIiwiYWNjZXNzTGV2ZWwiOiJtYXJrZXRwbGFjZSIsImxvZ2luVHlwZSI6ImJ1aWx0aW4iLCJ3YWxsZXRBZGRyZXNzIjoiMHg5ZjlmNTI0NWM4ZmNlY2QwNDdkNWJhMzE0YjJjZmY1MmIzMmY4NWI1In0sImlhdCI6MTY3NjczNzI2OSwiZXhwIjoxNjc5MzI5MjY5fQ.QRiUf6f-j7myeRNW5XJ-3uXOypnZBLrzuNHfF0DyUSM'
+// Cookie: tsb_www_token
+const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiOTlmNTQ3ZGMtZmUwYS00ZGEyLWI3ZTQtZTgzZmUzZjA0OWRkIiwiYWNjZXNzTGV2ZWwiOiJtYXJrZXRwbGFjZSIsImxvZ2luVHlwZSI6ImJ1aWx0aW4iLCJ3YWxsZXRBZGRyZXNzIjoiMHg5ZjlmNTI0NWM4ZmNlY2QwNDdkNWJhMzE0YjJjZmY1MmIzMmY4NWI1In0sImlhdCI6MTY3ODgyNTIxOCwiZXhwIjoxNjgxNDE3MjE4fQ.b01Cx4kVcUt9naKMEbsj5JioM_U6INFf9G1rSgeBJ58'
 const WPTService = new WPT();
 const dateString = new Date().toLocaleDateString("en-US", {
   year: "numeric",
@@ -10,15 +11,27 @@ const dateString = new Date().toLocaleDateString("en-US", {
   day: "numeric",
 });
 
-const pathsToTest = [
+const homePaths = [
+  // Home
   "",
-  "season/",
-  "season/contests/",
-  "season/contests/february-festival/",
-  "season/changelog/",
+]
+const seasonPaths = [
+  // Season
+  // "season/",
+  // "season/contests/",
+  // "season/contests/february-festival/",
+  // "season/changelog/",
+  "events/",
+  "events/march-festival/",
+  "events/march-festival/play/"
+]
+const mapPaths = [
+  // Map
   "map/",
+]
+const marketPaths = [
+  // Market
   "shop/",
-  "assets/",
   "assets/monowheel/0ca92f52-d693-4fbd-82eb-df1c7db3c4d9/",
   "collections/",
   "collections/parisland/200/",
@@ -26,9 +39,14 @@ const pathsToTest = [
   "avatars/octohedz-vx/206/",
   "nft/",
   "nft-lands/",
-  "create/game-maker/",
+]
+const blogPaths = [
+  // Blog
   "blog/",
   "blog/1000-sports-land-nfts-sold-in-less-than-24-hours/3279/",
+]
+const userPaths = [
+  // User
   "me/profile/",
   "me/claim/",
   "me/defi/",
@@ -38,18 +56,40 @@ const pathsToTest = [
   "me/claim/",
   "me/avatar/",
   "me/settings/",
+]
+const createPaths = [
+  // Create
   "create/game-maker/",
   "create/vox-edit/",
-  "blog/",
-  "blog/escape-to-paris-hiltons-tropical-paradise-with-parisland-valentines-day-experience-in-the-sandbox/3294/",
-  "about/"
+]
+const aboutPaths = [
+  // About
+  "about/",
+]
+const landsalePaths = [
+  // Landsale
+  "landsale/"
 ];
+
+const pathsToTest = [
+  ...homePaths,
+  ...seasonPaths,
+  ...mapPaths,
+  ...marketPaths,
+  ...blogPaths,
+  ...userPaths,
+  ...createPaths,
+  ...aboutPaths,
+  ...landsalePaths
+]
+
+const DESCRIPTION = ''
 
 pathsToTest.forEach((path) => {
   WPTService.runTest({
     config: {
       url: `www.sandbox.game/en/${path}`,
-      label: `${path || "home"} - Prod - ${dateString} - Pre First Sync`,
+      label: `${path.replace(/\//g, '.') || "home"} - Prod - ${dateString}${DESCRIPTION ? ` - ${DESCRIPTION}` : ''}`,
       // block:
       //   "https://www.sandbox.game/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js",
       pingback:
